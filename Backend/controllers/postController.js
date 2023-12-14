da3c46afd756fc812875ab065835ebcaa2c972c5
Yasiru-Deshan <yasirudeshan3@gmail.com>
2023-12-14 19:39:30 +0530

    crud functionalities related to posts added

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -23,6 +23,7 @@ const createPost = async (req, res, next) => {
   }
 };
 
+//get posts
 const getPosts = async (req, res, next) => {
   try {
     const posts = await Post.find();
@@ -35,5 +36,77 @@ const getPosts = async (req, res, next) => {
   }
 };
 
+//get post by id
+const getPostById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const post = await Post.findById(id);
+
+    return res.status(200).json({ msg: "Post found", post });
+  } catch (err) {
+    return res.status(500).json({
+      msg: err,
+    });
+  }
+};
+
+//edit post
+const updatePost = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const post = await Post.findById(id).populate('author');
+    
+    if (post) 
+    {
+      if (post.author._id == req.user.id)
+      {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("Post has been updated.");
+      } 
+      else 
+      {
+        res.status(200).json("You are not allowed to edit this post.");
+      }
+    }
+    else
+    {
+      return res.status(404).json({ msg: "Cannot find a post for this id" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+//delete a post
+const deletePost = async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const post = await Post.findById(id).populate("author");
+
+    if (post) 
+    {
+      if (post.author._id == req.user.id) 
+      {  
+        await post.deleteOne();
+        res.status(200).json("Post has been deleted.");
+      } else {
+        res.status(200).json("You are not allowed to remove this post.");
+      }
+    }
+    else
+    {
+      return res.status(404).json({ msg: "Cannot find a post for this id" });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      msg: err,
+    });
+  }
+};
+
 exports.createPost = createPost;
 exports.getPosts = getPosts;
+exports.getPostById = getPostById;
+exports.updatePost = updatePost;
+exports.deletePost = deletePost;
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   createPost,
   getPosts,
+  getPostById,
+  updatePost,
+  deletePost,
 } = require("../controllers/postController");
 const Authentication = require("../middleware/authentication");
 const router = express.Router();
 
 router.post("/new", Authentication, createPost);
 router.get("/", Authentication, getPosts);
+router.get("/:id", Authentication, getPostById);
+router.put("/:id", Authentication, updatePost);
+router.delete("/:id", Authentication,deletePost);
 
 module.exports = router;
